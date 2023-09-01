a6dbf8e0cc51ebac5ab17c6a2966003b4d589f1e
Joacker <jfernandezvizcarra@gmail.com>
2023-09-01 14:59:56 -0400

    added files for grpc example

--- Ayu1/backend/client/main.py
+++ Ayu1/backend/client/main.py
@@ -2,13 +2,11 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import grpc, json
 from google.protobuf.json_format import MessageToJson
-import example_pb2
-import example_pb2_grpc
+import examples_pb2
+import examples_pb2_grpc
 
 app = Flask(__name__)
 CORS(app)
-
-app.secret_key = 'un-secreto-shhhhh'
 
 class DataClient(object):
     def __init__(self):
@@ -16,18 +14,22 @@ class DataClient(object):
         self.host = 'server'
         self.server_port = '50051'
         self.channel = grpc.insecure_channel('{}:{}'.format(self.host, self.server_port))
-        self.stub = example_pb2_grpc.DataStub(self.channel)
+        self.stub = examples_pb2_grpc.DataStub(self.channel)
 
     def GetTemperature(self):
-        request = example_pb2.Empty()
+        request = examples_pb2.Empty()
         return self.stub.GetWeatherData(request)
     
     def GetCoins(self):
-        request = example_pb2.Empty()
+        request = examples_pb2.Empty()
         return self.stub.GetCoinsData(request)
 
 
 # ROUTES
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({"message": "Hello world"})
+
 @app.route('/temperature', methods=['GET'])
 def getTemperatura():
     client = DataClient()
--- Ayu1/backend/server/main.py
+++ Ayu1/backend/server/main.py
@@ -1,45 +1,45 @@
 import grpc
-import example_pb2
-import example_pb2_grpc
-from config import database_auth
+import examples_pb2
+import examples_pb2_grpc
+#from config import database_auth
 from concurrent import futures
 
 
 
-class DataServer(example_pb2_grpc.Data):
+class DataServer(examples_pb2_grpc.Data):
     
     def GetWeatherData(self, request, context):
         print("GetWeatherData")
         #extract the more refrehing time of the db
-        cur.execute("SELECT * FROM weather where date = '"+ str(current_date) +"' order by id desc limit 1")
-        weather = cur.fetchall()
+        #cur.execute("SELECT * FROM weather where date = '"+ str(current_date) +"' order by id desc limit 1")
+        #weather = cur.fetchall()
         temperature = {
-            "id": int(weather[0][0]),
-            "temp": str(weather[0][1]),
-            "time": str(weather[0][2]),
-            "date": str(weather[0][3])
+            "id": 1,
+            "temp": "20.0",
+            "time": "12:00:00",
+            "date": "2020-01-01"
         }
-        temperature_response = example_pb2.WeatherResponse(**temperature)
+        temperature_response = examples_pb2.WeatherResponse(**temperature)
         return temperature_response
     
     def GetCoinsData(self, request, context):
         print("GetCoinsData")
-        cur.execute("SELECT * FROM cash where date = '"+ str(current_date) +"' order by id desc limit 1")
-        coins = cur.fetchall()
+        #cur.execute("SELECT * FROM cash where date = '"+ str(current_date) +"' order by id desc limit 1")
+        # coins = cur.fetchall()
         coins = {
-            "id": int(coins[0][0]),
-            "dolar": str(coins[0][1]),
-            "euro": str(coins[0][2]),
-            "uf": str(coins[0][3]),
-            "time": str(coins[0][4]),
-            "date": str(coins[0][5])
+            "id": 1,
+            "dolar": "800.0",
+            "euro": "900.0",
+            "uf": "10000.0",
+            "time": "12:00:00",
+            "date": "2020-01-01"
         }
-        coins_response = example_pb2.CoinsResponse(**coins)
+        coins_response = examples_pb2.CoinsResponse(**coins)
         return coins_response
         
 def main():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    example_pb2_grpc.add_DataServicer_to_server(DataServer(), server)
+    examples_pb2_grpc.add_DataServicer_to_server(DataServer(), server)
     #Exponer a cualquier IP en el puerto 50051
     server.add_insecure_port('[::]:50051')
     server.start()
@@ -49,6 +49,6 @@ def main():
 
 
 if __name__ == '__main__':
-    conn = database_auth.get_db()
-    cur = conn.cursor()
+    #conn = database_auth.get_db()
+    #cur = conn.cursor()
     main()
