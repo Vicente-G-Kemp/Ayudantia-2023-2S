1e98079d626c420ca8851752764155292f169635
Joacker <jfernandezvizcarra@gmail.com>
2023-10-13 01:45:52 -0300

    partition worked

--- Ayu2/consumers/consumers2.py
+++ Ayu2/consumers/consumers2.py
@@ -1,4 +1,4 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, TopicPartition
 import random
 
 servidores_bootstrap = 'kafka:9092'
@@ -8,11 +8,16 @@ grupo_consumidores = f'grupo_consumidores_{topics[0]}'
 
 # Configurar el consumidor con el group_id
 consumer = KafkaConsumer(
-    *topics,
+    bootstrap_servers=[servidores_bootstrap],
     group_id=grupo_consumidores,
-    bootstrap_servers=[servidores_bootstrap]
 )
 
-# Consumir mensajes de los topics elegidos
+# Especificar la partición desde la que queremos consumir
+particion_especifica = TopicPartition(topics[0], 0)  # Selecciona la partición 0 del topic "temperatura"
+
+# Asignar la partición específica al consumidor
+consumer.assign([particion_especifica])
+
+# Consumir mensajes de la partición asignada
 for msg in consumer:
-    print(f"Topic: {msg.topic}, Mensaje: {msg.value}")
+    print(f"Topic: {msg.topic}, Partición: {msg.partition}, Mensaje: {msg.value.decode('utf-8')}")
--- Ayu2/producers/producers2.py
+++ Ayu2/producers/producers2.py
@@ -7,8 +7,7 @@ import string
 servidores_bootstrap = 'kafka:9092'
 topic_temperatura = 'temperatura'
 
-productor = KafkaProducer(bootstrap_servers=[servidores_bootstrap], value_serializer=lambda x: dumps(x).encode('utf-8'))  
-# Se añadió value_serializer para simplificar el envío de mensajes JSON
+productor = KafkaProducer(bootstrap_servers=[servidores_bootstrap], value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 def generar_id():
     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=random.randint(1, 20)))
@@ -17,13 +16,15 @@ def enviar_temperatura():
     topic = topic_temperatura
     while True:
         temperatura = round(random.uniform(10, 30), 1)
+        id_mensaje = generar_id()
         mensaje = {
             "timestamp": int(time.time()),
-            "id": generar_id(),
+            "id": id_mensaje,
             "temperatura": temperatura
         }
-        productor.send(topic, mensaje)  # No es necesario convertir mensaje a JSON aquí, ya que se hace automáticamente con value_serializer
-        print('Enviando JSON:', mensaje)  # Se imprime el diccionario mensaje directamente para mejorar la legibilidad
+        productor.send(topic, value=mensaje, key=id_mensaje.encode())  # Se especifica la clave de partición con el parámetro key
+        print(id_mensaje.encode())
+        print('Enviando JSON:', mensaje)
         time.sleep(3)
 
 if __name__ == "__main__":
