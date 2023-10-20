032fbf1750b01bedc9737425bd44abaaac730ffe
Joacker <jfernandezvizcarra@gmail.com>
2023-10-19 22:49:52 -0300

    new commit

--- Ayu2/consumers/consumers3.py
+++ Ayu2/consumers/consumers3.py
@@ -1,18 +1,26 @@
 from kafka import KafkaConsumer
-import random
+import json  # Agrega la importación de json para deserializar los mensajes
 
 servidores_bootstrap = 'kafka:9092'
 topics = ['temperatura', 'porcentaje_humedad', 'posicion']
 
-grupo_consumidores = f'grupo_consumidores_{random.choice(topics)}'
+grupo_consumidores = 'grupo_consumidores'
 
-# Configurar el consumidor con el group_id
+# Configurar el consumidor con el group_id y añadir deserializador de json
 consumer = KafkaConsumer(
     *topics,
     group_id=grupo_consumidores,
-    bootstrap_servers=[servidores_bootstrap]
+    bootstrap_servers=[servidores_bootstrap],
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))  
 )
 
 # Consumir mensajes de los topics elegidos
 for msg in consumer:
-    print(f"Topic: {msg.topic}, Mensaje: {msg.value}")
+    print(f"Recibido de tópico {msg.topic}: {msg.value}")
+
+    if msg.topic == 'temperatura':
+        print(f"Procesando temperatura: {msg.value['temperatura']}°C")
+    elif msg.topic == 'porcentaje_humedad':
+        print(f"Procesando humedad: {msg.value['porcentaje_humedad']}%")
+    elif msg.topic == 'posicion':
+        print(f"Procesando posición: {msg.value['posicion']}")
